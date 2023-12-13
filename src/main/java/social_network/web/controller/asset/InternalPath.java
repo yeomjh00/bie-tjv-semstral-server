a352bc5365f5994845364d5a47a2baa817881f53
James Yeom <duawognpc@gmail.com>
2023-12-13 21:37:37 +0100

    feat: User Register form, constants for URL

---
+++ src/main/java/social_network/web/controller/asset/InternalPath.java
@@ -0,0 +1,7 @@
+package social_network.web.controller.asset;
+
+public final class InternalPath {
+    public static final String home = "/";
+    public static final String userRegister = "users/new";
+    public static final String allUsers = "users/userList";
+}
---
+++ src/main/java/social_network/web/controller/asset/UserRegisterForm.java
@@ -0,0 +1,10 @@
+package social_network.web.controller.asset;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class UserRegisterForm {
+    private String username;
+    private String realName;
+}
