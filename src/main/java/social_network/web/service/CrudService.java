af06aaaba7a047a6a2ad20ee507073c977a0b1bb
James Yeom <duawognpc@gmail.com>
2023-12-13 20:30:44 +0100

    feat: CrudService Interface, Basic UserService Implementation
    UserService: Basic CRUD, another find methods

--- src/main/java/social_network/web/service/CrudService.java
+++ src/main/java/social_network/web/service/CrudService.java
@@ -1,28 +1,17 @@
 package social_network.web.service;
 
 import social_network.web.repository.CrudRepository;
+import social_network.web.repository.UserRepository;
 
 import java.util.Optional;
 
-public class CrudService<T, ID>{
-    private final CrudRepository<T, ID> repository;
+public interface CrudService<T, ID> {
 
-    public CrudService(CrudRepository<T, ID> repository) {
-        this.repository = repository;
-    }
+    public T save(T entity);
 
-    public T save(T entity) {
-        return null;
-    }
+    public Optional<T> findById(ID id);
 
-    public Optional<T> findById(ID id) {
-        return null;
-    }
+    public Iterable<T> findAll();
 
-    public Iterable<T> findAll() {
-        return null;
-    }
-
-    public void deleteById(ID id) {
-    }
+    public void deleteById(ID id);
 }
--- src/main/java/social_network/web/service/UserService.java
+++ src/main/java/social_network/web/service/UserService.java
@@ -1,27 +1,50 @@
 package social_network.web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import social_network.web.domain.User;
 import social_network.web.repository.UserRepository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
 @Transactional
-public class UserService {
+public class UserService implements CrudService<User, Long> {
     private final UserRepository userRepository;
     public UserService(@Autowired UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    public User join(User user) {
-        return userRepository.save(user);
+    @Override
+    public User save(User user) {
+        userRepository.save(user);
+        return user;
     }
 
-    public User findByUsername(String username) {
-        return userRepository.findByUsername(username).orElse(null);
+    @Override
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
     }
 
-    public User findByRealName(String realName) {
-        return userRepository.findByRealName(realName).orElse(null);
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+        return;
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public Optional<User> findByRealName(String realName) {
+        return userRepository.findByRealName(realName);
     }
 
 }
