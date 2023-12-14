6ef8c7dd5896b3341c2569e5726b8ddba4b3ee81
Jaehu Yeom <yeomjaeh@fit.cvut.cz>
2023-12-14 14:29:57 +0100

    fix: jpa repo to interface

--- src/main/java/social_network/web/repository/UserRepository.java
+++ src/main/java/social_network/web/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package social_network.web.repository;
 
+import org.springframework.stereotype.Repository;
 import social_network.web.domain.User;
 
 import java.util.Optional;
 
-public interface UserRepository extends CrudRepository<User, Long>{
+public interface UserRepository extends CrudRepository<User, Long>{ //
     Optional<User> findByUsername(String username);
 
     Optional<User> findByRealName(String realName);
