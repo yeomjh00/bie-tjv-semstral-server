80edd6de3fefade17ac7afe059db4a04349e0cda
James Yeom <yeomjaeh@cvut.cz>
2024-01-04 04:00:19 +0100

    feat: Media Controllers

--- src/main/java/social_network/web/controller/MusicListRestController.java
+++ src/main/java/social_network/web/controller/MusicListRestController.java
@@ -2,30 +2,86 @@ package social_network.web.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import social_network.web.controller.asset.MusicListDto;
+import social_network.web.domain.MusicList;
+import social_network.web.domain.User;
+import social_network.web.service.MusicListService;
+import social_network.web.service.UserService;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @RestController
-@RequestMapping("/{user_id}/music_list")
+@RequestMapping("/musiclists")
 public class MusicListRestController {
-//  private final UserService userService;
-//    private final MusicListService musicListService;
-//
-//    @Autowired
-//    public MusicListRestController(UserService userService,
-//    MusicListService musicListService){
-//        this.userService = userService;
-//        this.musicListService = musicListService;
-//    }
-//
-//    @GetMapping
-//    public List<MusicListDto> readAllMusicLists(@PathVariable Long user_id){
-//        log.info("read all music lists");
-//        return musicListService.findAll().stream()
-//                .map(MusicListDto::MusicList2Dto)
-//                .toList();
-//    }
+  private final UserService userService;
+    private final MusicListService musicListService;
 
+    @Autowired
+    public MusicListRestController(UserService userService,
+                                   MusicListService musicListService){
+        this.userService = userService;
+        this.musicListService = musicListService;
+    }
+
+    @GetMapping
+    public List<MusicListDto> readAllMusicLists(){
+        log.info("read all music lists");
+        return musicListService.findAll().stream()
+                .map(MusicListDto::MusicList2Dto)
+                .toList();
+    }
+
+    @PostMapping
+    public void createMusicList(@RequestBody MusicListDto musicListDto){
+        log.info("create music list: {}", musicListDto);
+        musicListService.saveFromDto(musicListDto);
+    }
+
+    @GetMapping("/{music-list-id}")
+    public MusicListDto readMusicListById(@PathVariable("music-list-id") Long mlId){
+        log.info("read music list by id: {}", mlId);
+        return MusicListDto.MusicList2Dto(musicListService.findById(mlId).orElse(null));
+    }
+
+    @PutMapping("/{music-list-id}")
+    public void updateMusicList(@PathVariable("music-list-id") Long mlId,
+                                @RequestBody MusicListDto musicListDto){
+        log.info("update music list: {}", musicListDto);
+        Optional<User> owner = userService.findById(musicListDto.getOwnerId());
+        if (owner.isEmpty()){
+            log.info("user not found");
+            return;
+        }
+        musicListService.updateFromDto(musicListDto);
+    }
+
+    @DeleteMapping("/{music-list-id}")
+    public void deleteMusicListById(@PathVariable("music-list-id") Long mlId){
+        log.info("delete music list by id: {}", mlId);
+        musicListService.deleteById(mlId);
+    }
+
+    @GetMapping("/owned")
+    public List<MusicListDto> readAllMusicListsByOwnerId(@RequestParam Long user_id){
+        log.info("read all music lists by owner id: {}", user_id);
+        return musicListService.findAllByOwnerId(user_id).stream()
+                .map(MusicListDto::MusicList2Dto)
+                .toList();
+    }
+
+    @GetMapping("/count-list")
+    public Long countListsByUserId(@RequestParam Long user_id){
+        log.info("count music by music list id: {}", user_id);
+        return musicListService.countListByUserId(user_id);
+    }
+
+    @GetMapping("/count-music")
+    public Long countMusicByMusicListId(@RequestParam Long list_id){
+        log.info("count music by music list id: {}", list_id);
+        return musicListService.countMusicByMusicListId(list_id);
+    }
 }
--- src/main/java/social_network/web/controller/PictureRestController.java
+++ src/main/java/social_network/web/controller/PictureRestController.java
@@ -2,18 +2,67 @@ package social_network.web.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import social_network.web.controller.asset.PictureDto;
+import social_network.web.domain.Picture;
+import social_network.web.service.PictureService;
+import social_network.web.service.PostService;
+import social_network.web.service.UserService;
+
+import java.util.List;
 
 @Slf4j
 @RestController
+@RequestMapping("/pictures")
 public class PictureRestController {
-//    private final UserService userService;
-//    private final PictureService pictureService;
-//
-//    @Autowired
-//    public PictureRestController(UserService userService,
-//    PictureService pictureService){
-//        this.userService = userService;
-//        this.pictureService = pictureService;
-//    }
+    private final PostService postService;
+    private final PictureService pictureService;
+
+    @Autowired
+    public PictureRestController(PostService postService,
+                                 PictureService pictureService){
+        this.postService = postService;
+        this.pictureService = pictureService;
+    }
+
+    @GetMapping
+    public List<PictureDto> readAllPictures(){
+        log.info("read all pictures");
+        return pictureService.findAll().stream()
+                .map(PictureDto::Picture2Dto)
+                .toList();
+    }
+
+    @PostMapping
+    public void createPicture(@RequestBody PictureDto pictureDto){
+        log.info("create picture: {}", pictureDto);
+        pictureService.save(Picture.Dto2Picture(pictureDto));
+    }
+
+    @GetMapping("/{picture-id}")
+    public PictureDto readPictureById(@PathVariable("picture-id") Long pId){
+        log.info("read picture by id: {}", pId);
+        return PictureDto.Picture2Dto(pictureService.findById(pId).orElse(null));
+    }
+
+    @PutMapping("/{picture-id}")
+    public void updatePicture(@PathVariable("picture-id") Long pId,
+                              @RequestBody PictureDto pictureDto){
+        log.info("update picture: {}", pictureDto);
+        pictureService.save(Picture.Dto2Picture(pictureDto));
+    }
+
+    @DeleteMapping("/{picture-id}")
+    public void deletePictureById(@PathVariable("picture-id") Long pId){
+        log.info("delete picture by id: {}", pId);
+        pictureService.deleteById(pId);
+    }
+
+    @GetMapping("/contained")
+    public List<PictureDto> readAllPicturesByContainedPostId(@RequestParam Long post_id){
+        log.info("read all pictures by contained post id: {}", post_id);
+        return pictureService.findAllByPostId(post_id).stream()
+                .map(PictureDto::Picture2Dto)
+                .toList();
+    }
 }
